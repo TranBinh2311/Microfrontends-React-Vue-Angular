d778c1e1030ac7076ed88cd8a57b82b8a1286afa
Tranbinh2311 <binhlinh473@gmail.com>
2023-06-04 00:40:57 +0700

    ci/cd: Added marketing deployment

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -2,7 +2,7 @@ const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
-const packageJson = require("../package.json");
+const packageJson = require('../package.json');
 
 const domain = process.env.PRODUCTION_DOMAIN;
 
@@ -16,7 +16,7 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
