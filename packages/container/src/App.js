621f1ffd19b4eb4248f0fdd48b8089ff12b876d0
Tranbinh2311 <binhlinh473@gmail.com>
2023-06-04 00:48:12 +0700

    feature: update UI

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,8 +3,7 @@ import Marketing from "./component/Marketing";
 export default () => {
   return (
     <div>
-      <h1>Hi there! My name is Binh. I am from ...</h1>
-      <hr/>
+      <h1>This is test function of MFE</h1>
       <hr/>
       <Marketing/>
     </div>
